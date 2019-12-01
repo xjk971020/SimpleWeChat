1027a6e039bac853ff1514ff883b413614d49d8d
xjk971020 <823840954@qq.com>
2019-12-01 16:57:02 +0800

    swagger接口文档配置

---
+++ src/main/java/com/cathetine/simpleChat/config/SwaggerConfiguration.java
@@ -0,0 +1,37 @@
+package com.cathetine.simpleChat.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * @Author:xjk
+ * @Date 2019/12/1 16:56
+ * swagger接口文档
+ */
+@Configuration
+public class SwaggerConfiguration {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("cn.dezhi.welfare.partner.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("颐德尼喜 合作伙伴子系统 api文档")
+                .description("简单优雅的restful风格")
+                .version("1.0")
+                .build();
+    }
+
+}
